576716b00af209529bd2e7c4a24c844c370aab1b
Marcin Bieniek <marcingim@gmail.com>
2022-12-16 23:31:50 +0100

    Add selected hours data agregation & styling

--- app/src/modules/CalendarBox/CalendarBox.js
+++ app/src/modules/CalendarBox/CalendarBox.js
@@ -1,19 +1,27 @@
 import CalendarContext from "../../providers/CalendarProvider";
 import { useContext } from "react";
+import { useState } from "react";
+import { useCallback } from "react";
 
 const CalendarBox = () => {
   
     const { calendarData } = useContext(CalendarContext);
 
-    const weekList = calendarData.map((item, weekIndex) => 
-        <div className="day-column">
-            <h2 key={weekIndex}>{item.dayName}</h2>
-            {item.hours.map((hour, hourIndex) => 
-                <div key={hourIndex} className="hour">{hour}
-                </div>         
-            )}
-        </div>
-    )
+    const [reservedHour, setReservedHour] = useState('');
+    const [reservedDay, setReservedDay] = useState('');
+
+    const handleSetReservedDay = (e, day) => {
+        e.preventDefault()
+        setReservedDay(day)
+    }
+
+    const handleSetReservedHour = (e, hour) => {
+        e.preventDefault()
+        setReservedHour(hour)
+    }
+
+    console.log('reserved day is ', reservedDay)
+    console.log('reserved hour is ', reservedHour)
 
     return ( 
         <div className="calendar-container">
@@ -22,7 +30,26 @@ const CalendarBox = () => {
             <div className="calendar-content">
                 <h1>Okres czasu: 14.11 - 20.11 (46)</h1>
                 <div className="calendar-days"> 
-                    {weekList}  
+                    {calendarData.map((item, weekIndex) => 
+                        <div className="day-column" >
+                            <h2 key={weekIndex}>{item.dayName}</h2>
+                            {item.hours.map((hour, hourIndex) => 
+                                <div 
+                                    key={hourIndex} 
+                                    onClick={(e) => {
+                                        handleSetReservedDay(e, item.dayName)
+                                        handleSetReservedHour(e, hour)
+                                    }}  
+                                    className=   
+                                    {`hour ${
+                                        reservedHour === hour && 
+                                        reservedDay === item.dayName
+                                        ? "active" : ""}`}       
+                                >{hour}
+                                </div>       
+                            )}
+                        </div>
+                    )}  
                 </div>
                 <button>Zapisz tydzień</button>
             </div>
